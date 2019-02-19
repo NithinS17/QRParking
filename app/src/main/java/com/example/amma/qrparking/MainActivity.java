5be03f5884dac98becdc01a6892e06325c3d08c2
MadBro <sreeragabc1998@gmail.com>
2019-02-19 23:25:50 +0530

    new commits

--- app/src/main/java/com/example/amma/qrparking/MainActivity.java
+++ app/src/main/java/com/example/amma/qrparking/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.amma.qrparking;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -16,10 +17,12 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
