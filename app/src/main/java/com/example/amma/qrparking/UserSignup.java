e5075d7b69f58059b687377144d6c7e8b25cf819
ayesweyen <aswindsunil@gmail.com>
2019-02-27 15:33:45 +0530

    upto database

--- app/src/main/java/com/example/amma/qrparking/UserSignup.java
+++ app/src/main/java/com/example/amma/qrparking/UserSignup.java
@@ -1,14 +1,31 @@
 package com.example.amma.qrparking;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class UserSignup extends AppCompatActivity {
     EditText user,pass,repass,mob;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference dbref = database.getReference("user");
+    FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +36,7 @@ public class UserSignup extends AppCompatActivity {
         pass = (EditText)findViewById(R.id.pass);
         mob = (EditText)findViewById(R.id.mob);
         Button login=(Button) findViewById(R.id.lin);
+
 
 
         login.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +59,51 @@ public class UserSignup extends AppCompatActivity {
                     mob.requestFocus();
                 }
                 else{
+
+                    dbref.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+                            int nousr = Integer.parseInt(String.valueOf(dataSnapshot.getValue()));
+                            dbref.setValue(nousr + 1);
+                            nousr = (nousr + 1);
+                            DatabaseReference usrRef = database.getReference(dbref.getKey() + nousr);
+                            usrRef.child("Name").setValue(user.getText().toString());
+                            usrRef.child("Phone").setValue(mob.getText().toString());
+                            usrRef.child("Password").setValue(pass.getText().toString());
+                            Toast.makeText(UserSignup.this, "Successfully Registered", Toast.LENGTH_LONG).show();
+
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            Toast.makeText(UserSignup.this, "Unable to connect. Please try again later.", Toast.LENGTH_LONG).show();
+
+                        }
+                    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                     Intent i = new Intent(getApplicationContext(),UserSignup.class);
                     startActivity(i);
                 }
--- app/src/main/java/com/example/amma/qrparking/admin.java
+++ app/src/main/java/com/example/amma/qrparking/admin.java
@@ -6,12 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 
 public class admin extends AppCompatActivity {
     //int f;
     EditText user,pass;
+
+
 
 
 
@@ -37,7 +44,7 @@ public class admin extends AppCompatActivity {
                    pass.requestFocus();
                }
                 else{
-                    Intent i = new Intent(getApplicationContext(),admin.class);
+                    Intent i = new Intent(getApplicationContext(),AdminHome.class);
                     startActivity(i);
                 }
            }
